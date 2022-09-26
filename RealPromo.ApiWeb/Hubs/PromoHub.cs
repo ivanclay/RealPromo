5af069be37a9b77d39874aca8b236ac55cc33159
ivanclay <ivan.clay.moura@gmail.com>
2022-09-26 08:20:37 -0300

    feat: first commit real promo signalR project.

---
+++ RealPromo.ApiWeb/Hubs/PromoHub.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace RealPromo.ApiWeb.Hubs
+{
+    public class PromoHub: Hub
+    {
+    }
+}
---
+++ RealPromo.ApiWeb/Models/Promocao.cs
@@ -0,0 +1,11 @@
+﻿namespace RealPromo.ApiWeb.Models
+{
+    public class Promocao
+    {
+        public int Id { get; set; }
+        public string Empresa { get; set; }
+        public string Chamada { get; set; }
+        public string Regras { get; set; }
+        public string EnredecoURL { get; set; }
+    }
+}
